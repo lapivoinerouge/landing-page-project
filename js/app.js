9f5ec2cb82d7ad294acb1caf751d9fda95507988
lapivoinerouge <wioladys@gmail.com>
2020-04-11 16:10:48 +0200

    media breakpoints css

--- js/app.js
+++ js/app.js
@@ -1,18 +1,10 @@
-/**
- * Define Global Variables
- * 
-*/
 const navList = document.getElementById("navbar__list");
 const navbar = document.getElementById("nav-menu");
 const activeClassName = "your-active-class";
+let topButton;
 let activeClass = document.getElementsByClassName(activeClassName).item(0);
 let activeTitle = null;
 
-/**
- * End Global Variables
- * Start Functions
- * 
-*/
 // highlight picked title
 function underlineTitle(title) {
     if (activeTitle != null) {
@@ -29,16 +21,18 @@ function setActiveSection(section) {
     activeClass = section;
 }
 
-// build nav + button
+// create button
+function createButton() {
+    topButton = document.createElement("button");
+    topButton.setAttribute("id", "scroll-up");
+    topButton.textContent = "back to top";
+    const header = document.querySelector("header");
+    header.insertAdjacentElement("afterend", topButton);  
+}
+
+// build nav
 document.addEventListener("DOMContentLoaded", function() {
     navbar.style.cssText = "background-color: #000d3c; height: 100px;";
-    navList.style.cssText = "display: flex; flex-direction: row; font-size: 25px; justify-content: space-evenly;";
-    
-    const button = document.createElement("button");
-    button.setAttribute("id", "scroll-up");
-    button.textContent = "back to top";
-    const header = document.querySelector("header");
-    header.insertAdjacentElement("afterend", button);
 });
 
 // build menu
@@ -77,15 +71,18 @@ for (let i = 1; i < containerSize + 1; i++) {
     });
 };
 
-
 // show/hide button
+createButton();
 window.addEventListener('scroll', function() {
     let windowTop = window.pageYOffset;
-    const button = document.getElementById("scroll-up");
     if (windowTop > 500) {
-        button.style.opacity = "100%";
+        topButton.style.display = "block";
     } else {
-        button.style.opacity = "0";
+        topButton.style.display = "none";
     }
 });
 
+// scroll to top with button
+topButton.addEventListener("click", function() {
+    document.documentElement.scrollTop = 0;
+});
